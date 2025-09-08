5ffa0faf904e0429f04e2afec8d22d3e78207c44
AleTavares <portalatibaia@gmail.com>
2025-09-08 16:05:18 -0300

    Devolvendo o erro do teste

--- app/calc.py
+++ app/calc.py
@@ -1,6 +1,6 @@
 def soma(a, b):
-    # return a - b  # ERRO proposital
-    return a + b  # Correção
+    return a - b  # ERRO proposital
+    # return a + b  # Correção
 
 def multiplica(a, b):
     return a * b
