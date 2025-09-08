ae222ede445b3884d54a4d8e67e98bc4be485fe3
AleTavares <portalatibaia@gmail.com>
2025-09-08 16:00:34 -0300

    Ajuste erro de TesteUnitário

--- app/calc.py
+++ app/calc.py
@@ -1,5 +1,6 @@
 def soma(a, b):
-    return a - b  # ERRO proposital
+    # return a - b  # ERRO proposital
+    return a + b  # Correção
 
 def multiplica(a, b):
     return a * b
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,8 +1,9 @@
 import subprocess
+import sys
 
 def test_python_in_docker():
     result = subprocess.run(
-        ["python", "-c", "print('Hello from Docker!')"],
+        [sys.executable, "-c", "print('Hello from Docker!')"],
         capture_output=True, text=True
     )
     assert "Hello from Docker!" in result.stdout
